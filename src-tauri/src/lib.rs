919c1b72ebfde46df227a3c5fb59bd9a0cb467e2
vrontinos <eshop@vrontinos.gr>
2026-05-03 01:47:49 +0300

    release 1.0.41

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -19,7 +19,11 @@ pub fn run() {
         .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
             show_main_window(app);
         }))
-        .plugin(tauri_plugin_autostart::Builder::new().build())
+        .plugin(
+    	 tauri_plugin_autostart::Builder::new()
+        .args(["--autostart"])
+        .build(),
+)
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .setup(|app| {
